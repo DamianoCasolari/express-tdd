722a62a1fe69bf062e61ef2829c571e8dc36f076
DamianoCasolari <damianocasolari@gmail.com>
2023-11-20 14:00:13 +0100

    add last three tasks

--- test/createSlug.test.js
+++ test/createSlug.test.js
@@ -1,25 +1,47 @@
-const{test, expect} = require("@jest/globals")
+const { test, expect } = require("@jest/globals")
 const createSlug = require("../utilities/createSlug")
 
 
-test("dovrebbe restituire una stringa", ()=>{
-    const text = {marco:"ciccio", maria:"ciccia"}
+test("dovrebbe restituire una stringa", () => {
+    const text = "Hello Hell"
     const result = createSlug(text)
 
     expect(typeof result).toBe("string")
 })
 
-
-test("dovrebbe restituire una stringa in lowercase", ()=>{
+test("dovrebbe restituire una stringa in lowercase", () => {
     const text = "maRco-Se-la-Comanda"
     const result = createSlug(text)
 
     expect(result).toBe("marco-se-la-comanda")
 })
 
-test("dovrebbe ritornare una stringa con gli spazi sostituiti da -", ()=>{
+test("dovrebbe ritornare una stringa con gli spazi sostituiti da -", () => {
     const text = "marco se la comanda"
     const result = createSlug(text)
-
     expect(result).toBe("marco-se-la-comanda")
+})
+
+test("dovrebbe incrementare di 1 lo slug quando esiste già -", () => {
+    const text = "ciao mondo"
+    const result = createSlug(text)
+
+    expect(result).toMatch(/^ciao-mondo-\d+$/)
+})
+
+test("dovrebbe lanciare un errore in caso di titolo non presente o formato errato", () => {
+
+    const result = () => createSlug({ marco: "ciccio", maria: "ciccia" })
+    expect(result).toThrowError("The slug is required and must be a string")
+})
+
+test("dovrebbe lanciare un errore se manca l’array dei post", () => {
+    const posts = require("../db/emptyPosts.json")
+
+    const testFunction = () => {
+        if (!Array.isArray(posts) || posts.length <= 1) {
+            throw new Error("posts is missing or not an array");
+        }
+    };
+    expect(testFunction).toThrowError("posts is missing or not an array")
 })
--- utilities/createSlug.js
+++ utilities/createSlug.js
@@ -1,7 +1,27 @@
 module.exports = function (text) {
+    const users = require("../db/posts.json")
 
+    // should throw an error in case of missing or invalid title 
+    if(!text || typeof text != "string"){
+        throw new Error("The slug is required and must be a string")
+    }
+
+    // should return a string and lowercase 
     let editText = text.toString().toLowerCase()
-    let kebabString = editText.split(" ").join("-")
+
+    // should return a string with spaces replaced by - 
+    let kebabString = editText.replaceAll(" ", "-")
+
+
+    // should increment the slug by 1 when it already exists 
+    const userWithSameSlug = users.find((user) => user.slug == kebabString)
+    if (userWithSameSlug) {
+        let index = 0
+        const arrayIndexes = users.map((user) => index++)
+        let maxIndex = Math.max(...arrayIndexes)
+        kebabString = kebabString + "-" + (maxIndex + 1)
+    }
+
 
 
     return kebabString
