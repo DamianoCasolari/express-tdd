1e9f171164d39460ba14e33b894fdd2d9c7398ee
DamianoCasolari <damianocasolari@gmail.com>
2023-11-22 21:50:10 +0100

    fix bug shadowCopy

--- models/model.js
+++ models/model.js
@@ -6,7 +6,7 @@ module.exports = class Model {
         if (!filePath) {
             throw new Error("The path of the file is required")
         }
-        this.filePath =  filePath;
+        this.filePath = filePath;
 
     }
     read() {
@@ -20,14 +20,9 @@ module.exports = class Model {
 
     add(name, age) {
         let arrayElements = this.read()
-
-        // let newElement = `{name: ${name}, age: ${age}}`
-        const newElement = {name,age}
-
+        const newElement = { name, age }
         arrayElements.push(newElement)
-
         fs.writeFileSync(this.filePath, JSON.stringify(arrayElements), "utf-8")
-
         return arrayElements;
     }
 }
--- test/model.test.js
+++ test/model.test.js
@@ -1,10 +1,10 @@
 const { test, expect } = require("@jest/globals")
-const model = require("../models/model")
 const Model = require("../models/model")
-const { read } = require("fs")
+const fs = require("fs")
+const path = require("path")
 
 test("Model dovrebbe essere una classe", () => {
-    expect(typeof model).toBe("function")
+    expect(typeof Model).toBe("function")
 })
 
 test("L’istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell’istanza", () => {
@@ -39,11 +39,13 @@ expect(isArray).toThrowError("The file is not an array")
 })
 
 test("add dovrebbe aggiungere un elemento all’array dei dati e ritornare tutta la lista", () => {
-    const instanceOfModel = new Model("../db_test/fakeFile.json");
-    const initialArray = instanceOfModel.read();
-    const newArrayList = instanceOfModel.add("marco",42); 
 
-    expect(Array.isArray(newArrayList)).toBe(true); 
-    expect(newArrayList).toContainEqual({ name: "marco", age: 42 }); 
-    expect(newArrayList).toHaveLength(initialArray.length + 1);
+    const istanceOfModel = new Model(path.resolve(__dirname,"../db_test/fakeFile.json" ))
+    const initialArray = istanceOfModel.read();
+    const deepCopy = [...initialArray]
+    const newArrayList = istanceOfModel.add("marco",42); 
+
+    expect(Array.isArray(initialArray)).toBe(true); 
+    expect(newArrayList).toContainEqual({ "name": "marco", "age": 42 }); 
+    expect(newArrayList).toHaveLength(deepCopy.length + 1);
 })
